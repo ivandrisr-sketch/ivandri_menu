8df773fd325ffaf4e6bd92013d411c621637575b
Ivan Dario Salcedo Rivera <ivandrisr@gmail.com>
2026-07-26 11:58:37 -0500

    Implement Office license status check script
    Added script to check Office license status using ospp.vbs.

--- Modules/Office.ps1
+++ Modules/Office.ps1
@@ -1,1 +1,27 @@
+"2"{
 
+Clear-Host
+
+Write-Host "Estado de licencia Office"
+
+$office = Get-ChildItem `
+"C:\Program Files\Microsoft Office" `
+-Recurse `
+-Filter ospp.vbs `
+-ErrorAction SilentlyContinue
+
+
+if($office){
+
+cscript.exe $office.FullName /dstatus
+
+}
+else{
+
+Write-Host "No se encontró herramienta Office"
+
+}
+
+Esperar
+
+}
